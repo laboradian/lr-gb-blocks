d15834f6b00a7776318630be2e828bfc70b4202a
kura5 <laboradian@gmail.com>
2019-05-18 17:56:02 +0900

    Commit files as the first step

---
+++ lr-gb-blocks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Laboradian Gutenberg Blocks
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Laboradian
+ * Author URI:      laboradian.com
+ * Text Domain:     lr-gb-blocks
+ * Domain Path:
+ * Version:         0.1.0
+ *
+ * @package         Lr_Gb_Blocks
+ */
+
+require_once __DIR__ . '/blocks/graybox.php';
+
